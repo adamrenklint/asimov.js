b2d86d66aba097549140fc3cdb01208d1933571c
adamrenklint <adam@renklint.com>
2014-02-25 22:14:43 +0100

    tests for Base class, and I think I solved the collection issue, at least it seems. time to go for something to eat, get some fresh air. bla bla bla :)

--- tests/core/Collection.test.js
+++ tests/core/Collection.test.js
@@ -1,13 +1,19 @@
 test('core/Collection', [
 
-  '../../lib/core/Collection'
+  '../../lib/core/Collection',
+  'lodash'
 
 ], function (runner) {
 
-  var instance;
+  var instance, _;
 
   runner.beforeEach(function () {
     instance = new runner.deps.Collection();
+    _ = runner.deps.lodash;
+  });
+
+  runner.afterEach(function () {
+    instance.destroy();
   });
 
   runner.spec('add (object model, object options)', function () {
@@ -16,18 +22,68 @@ test('core/Collection', [
 
       runner.it('should add model to self.models', function () {
 
-        var model = {
-          'foo': 'bar',
-          'id': 44
+        var attributes = {
+          'foo': 'bar'
         };
 
-        instance.add(model);
+        instance.add(attributes);
 
         expect(instance.models.length).to.equal(1);
-        expect(instance.models[0].id).to.equal(44);
+        expect(instance.models[0].attributes.foo).to.equal('bar');
       });
 
-      runner.it('should trigger "add" event, passing the model as argument');
+      runner.it('should trigger the models "add" event', function () {
+
+        var attributes = {
+          'foo': 'bar'
+        };
+        var spy = sinon.spy();
+        var model = new instance.model(attributes);
+        model.on('add', spy);
+        instance.add(model);
+
+        expect(spy).to.have.been.calledOnce;
+        model.destroy();
+      });
+
+      runner.it('should trigger the models "all" event', function () {
+
+        var attributes = {
+          'foo': 'bar'
+        };
+        var spy = sinon.spy();
+        var model = new instance.model(attributes);
+        model.on('all', spy);
+        instance.add(model);
+
+        expect(spy).to.have.been.calledOnce;
+        model.destroy();
+      });
+
+      runner.it('should listen for models "all" event and call self._onModelEvent()', function () {
+
+        var attributes = {
+          'foo': 'bar'
+        };
+        var spy = sinon.spy(instance, '_onModelEvent');
+        instance.add(attributes);
+
+        expect(spy).to.have.been.calledOnce;
+      });
+
+      runner.it('should trigger "add" event', function () {
+
+        var spy = sinon.spy();
+        var attributes = {
+          'foo': 'bar'
+        };
+
+        instance.on('add', spy);
+        instance.add(attributes);
+
+        expect(spy).to.have.been.calledOnce;
+        expect(spy.args[0][0].attributes.foo).to.equal('bar');
+      });
     });
   });
 });
