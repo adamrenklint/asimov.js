fee12b34a9206ce25ee72cd2a61c61189dac5df2
adamrenklint <adam@renklint.com>
2014-02-27 10:07:19 +0100

    stubbed more specs for stylesheet parser

--- tests/watcher/StyleSheetParser.test.js
+++ tests/watcher/StyleSheetParser.test.js
@@ -9,12 +9,29 @@ test('watcher/StyleSheetParser', [
   var instance, _;
 
   runner.beforeEach(function () {
-    instance = new runner.deps.StyleSheetParser();
+    instance = new runner.deps.StyleSheetParser({
+      'paths': {
+        'styles': ['lib/styles', 'site/styles']
+      }
+    });
     _ = runner.deps.lodash;
   });
 
   runner.afterEach(function () {
     instance.destroy();
+  });
+
+  runner.spec('initialize (object options)', function () {
+
+    runner.when('options.paths.styles is not an array', function () {
+
+      runner.it('should throw an error', function () {
+
+        expect(function () {
+          new runner.deps.StyleSheetParser();
+        }).to.throw(Error);
+      });
+    });
   });
 
   runner.spec('parse (object model, string raw, object dependencies)', function () {
@@ -72,7 +89,36 @@ test('watcher/StyleSheetParser', [
 
       runner.when('the imported stylus file exists', function () {
 
+        runner.it('should add itself to the dependency graph', function (done) {
+
+          var model = new runner.deps.Model({
+            'path': 'foo/bar',
+            'raw': '@import "404.styl"\n@import "main.styl"'
+          });
+          var dependencies = new runner.deps.Model();
+          instance.parse(model, null, dependencies);
+
+          expect(_.keys(model.attributes).length).to.equal(3);
+
+          _.each(dependencies.attributes, function (arr, path) {
+            if (path.indexOf('foo/bar') >= 0) {
+
+              // expect(arr.length).to.equal(1);
+              _.each(arr, function (dep) {
+                if (dep.attributes.path.indexOf('foo/bar') >= 0) {
+                  done();
+                }
+              });
+            }
+          });
+        });
+
         runner.it('should register the model as a node of the stylus file in the dependency graph');
+
+        runner.when('the found stylus file includes includes @import statements', function () {
+
+          runner.it('should register the first stylesheet as a dependency of the other');
+        });
       });
 
       runner.when('the imported stylus file does not exist', function () {
