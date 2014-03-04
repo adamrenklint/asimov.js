80effb7b5b64131c9b63c5070a4a42135639702c
adamrenklint <adam@renklint.com>
2014-03-04 23:17:15 +0100

    added failing tests for Children template helper

--- tests/helpers/Children.test.js
+++ tests/helpers/Children.test.js
@@ -9,15 +9,32 @@ test([
 
   beforeEach(function () {
 
-    var collection = new test.deps.Collection();
+    var pages = new test.deps.Collection([{
+      'type': 'page',
+      'url': '/'
+    }, {
+      'type': 'page',
+      'url': '/sub'
+    }, {
+      'type': 'page',
+      'url': '/sub/sub2'
+    }, {
+      'type': 'page',
+      'url': '/foo'
+    }, {
+      'type': 'page',
+      'url': '/foo/bar'
+    }]);
+
+    var queue = new test.deps.Collection();
 
     instance = new test.deps.Children({
       'name': 'active',
-      'queue': collection,
-      'pages': collection
+      'queue': queue,
+      'pages': pages
     });
 
-    instance.setUrl('/blog');
+    instance.setUrl('/sub');
   });
 
   test.spec('run (string url, object options)', function () {
@@ -28,19 +45,65 @@ test([
 
         test.when('options.fn is a function', function () {
 
-          test.it('should execute function with each child');
-          test.it('should return the value of all function calls');
+          test.it('should execute function with each child', function () {
+
+            var spy = sinon.spy();
+            instance.run('/foo', {
+              'hash': {},
+              'fn': spy
+            });
+
+            expect(spy).to.have.been.calledOnce;
+            expect(spy).to.have.been.calledWith({
+              'type': 'page',
+              'url': '/foo/bar'
+            });
+          });
+
+          test.it('should return the value of all function calls', function () {
+
+            var result = instance.run('/foo', {
+              'hash': {},
+              'fn': function (obj) {
+                return '>>' + obj.url;
+              }
+            });
+
+            expect(result).to.equal('>>/foo/bar');
+          });
+
+          test.it('should only include direct children', function () {
+
+            var result = instance.run('/', {
+              'hash': {},
+              'fn': function (obj) {
+                return '>>' + obj.url;
+              }
+            });
+
+            expect(result).to.equal('>>/sub>>/foo');
+          });
         });
 
         test.when('options.fn is not function', function () {
 
-          test.itShouldThrowError();
+          test.itShouldThrowError(function () {
+            instance.run('/foo', {
+              'hash': {},
+              'fn': null
+            });
+          });
         });
       });
 
       test.when('no page exists for url', function () {
 
-        test.itShouldThrowError();
+        test.itShouldThrowError(function () {
+            instance.run('/foos', {
+              'hash': {},
+              'fn': function () {}
+            });
+          });
       });
     });
 
@@ -48,16 +111,34 @@ test([
 
       test.when('options.url is a string', function () {
 
-        test.it('should use options.url as url');
-        test.it('should execute function with each child');
-        test.it('should return the value of all function calls');
+        test.it('should use options.url as url', function () {
+
+          var result = instance.run({
+            'hash': {
+              'url': '/foo'
+            },
+            'fn': function (obj) {
+              return '>>' + obj.url;
+            }
+          });
+
+          expect(result).to.equal('>>/foo/bar');
+        });
       });
 
       test.when('options.hash.url is not a string', function () {
 
-        test.it('should use self.currentUrl as url');
-        test.it('should execute function with each child');
-        test.it('should return the value of all function calls');
+        test.it('should use self.currentUrl as url', function () {
+
+          var result = instance.run({
+            'hash': {},
+            'fn': function (obj) {
+              return '>>' + obj.url;
+            }
+          });
+
+          expect(result).to.equal('>>/sub/sub2');
+        });
       });
     });
   });
