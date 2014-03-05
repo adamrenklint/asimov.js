ce38a44b3ae9a1aa0e0cd5bfe367c28b6b7d4d06
adamrenklint <adam@renklint.com>
2014-03-05 21:25:25 +0100

    is failure code coming across

--- tests/nodes/PageNode.test.js
+++ tests/nodes/PageNode.test.js
@@ -1,13 +1,18 @@
 test([
 
   '../../lib/nodes/PageNode',
+  '../../lib/nodes/PageNodesCollection',
   'lodash'
 
 ], function (test) {
 
-  var instance, _;
+  var instance, _, pages;
 
-  beforeEach(function () {
+  test.before(function (done) {
+
+  });
+
+  test.beforeEach(function () {
 
     _ = test.deps.lodash;
     instance = new test.deps.PageNode({
@@ -23,7 +28,7 @@ test([
     });
   });
 
-  test.spec('getUrl (string url)', function () {
+  test.spec('parseUrl (string url)', function () {
 
     test.it('should parse urls correctly', function () {
 
@@ -38,8 +43,50 @@ test([
       tests[getPath('/sub/sub2')] = '/sub/sub2';
 
       _.each(tests, function (value, key) {
-        var result = instance.getUrl(key);
+        var result = instance.parseUrl(key);
         expect(result).to.equal(value);
+      });
+    });
+  });
+
+  test.spec('children ()', function () {
+
+    test.when('self.pages is not an instance of PageNodesCollection', function () {
+
+      test.itShouldThrowError(function () {
+
+        instance.children();
+      });
+    });
+
+    test.when('a pages collection is defined', function () {
+
+      test.beforeEach(function () {
+
+        var pages = new test.deps.PageNodesCollection();
+        instance.mediator.trigger('collection:pages', pages);
+      });
+
+      test.it('should return an instance of PageNodesCollection', function () {
+
+        var children = instance.children();
+        expect(children).to.be.instanceOf(test.deps.PageNodesCollection);
+      });
+
+      test.when('the page model has children in self.pages', function () {
+
+        test.it('should return each child in the PageNodesCollection', function () {
+
+          // var children = instance.children();
+          // expect(children).to.be.instanceOf(test.deps.PageNodesCollection);
+        });
+
+        test.it('should not return any other pages in the PageNodesCollection');
+      });
+
+      test.when('the page model has no children in self.pages', function () {
+
+        test.it('should return an empty PageNodesCollection');
       });
     });
   });
