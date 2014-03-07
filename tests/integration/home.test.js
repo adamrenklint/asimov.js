a5ff089ef8c1073ea93dd100f1e05a59ee10c627
adamrenklint <adam@renklint.com>
2014-03-07 20:14:52 +0100

    now that's more like it

--- tests/integration/home.test.js
+++ tests/integration/home.test.js
@@ -6,20 +6,6 @@ test(function (test) {
     instance = null;
   });
 
-  test.spec('test (string name, array dependencies, function callback)', function () {
-
-    test.when('name is not a string', function () {
-
-      test.it('should throw an error', function () {
-
-        expect(function () {
-          instance.test(null, [], function () {});
-        }).to.throw(Error);
-      });
-    });
-
-  });
-
   test.integration('/', function () {
 
     test.itShould.loadPage();
