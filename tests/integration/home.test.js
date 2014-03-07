412cf5f5a57494544811a877349149aa34c4024d
adamrenklint <adam@renklint.com>
2014-03-07 20:52:14 +0100

    some silly integration tests

--- tests/integration/home.test.js
+++ tests/integration/home.test.js
@@ -9,5 +9,15 @@ test(function (test) {
   test.integration('/', function () {
 
     test.itShould.loadPage();
+
+    test.it('should have the correct title', function () {
+
+      expect(this.browser.text('H1')).to.contain('asimov.js');
+    });
+
+    test.it('should contain a link to asimovjs.org', function () {
+
+      expect(this.browser.html('a')).to.contain('href="http://asimovjs.org"');
+    });
   });
 });
