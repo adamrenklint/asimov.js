cd3d5758a4f437d54502549242ee734ae3ceb11e
adamrenklint <adam@renklint.com>
2014-03-22 22:18:21 +0100

    first new test passing, 10 more to go

--- tests/updaters/PageHandler.test.js
+++ tests/updaters/PageHandler.test.js
@@ -1,7 +1,7 @@
 test([
 
   '../../lib/updaters/PageHandler',
-  '../../lib/core/Model',
+  '../../lib/core/Collection',
   'lodash'
 
 ], function (test) {
@@ -9,7 +9,10 @@ test([
   var instance, _;
 
   test.beforeEach(function () {
-    instance = new test.deps.PageHandler();
+    instance = new test.deps.PageHandler({
+      'pages': new test.deps.Collection(),
+      'templates': new test.deps.Collection()
+    });
     _ = test.deps.lodash;
   });
 
@@ -19,7 +22,15 @@ test([
 
   test.spec('created (string path, array graph)', function () {
 
-    test.it('should call fetch() on self.options.pages');
+    test.it('should call fetch() on self.options.pages', function () {
+
+      var spy = sinon.spy(instance.options.pages, 'fetch');
+
+      instance.created('/any/path/page.txt', []);
+
+      expect(spy).to.have.been.calledOnce;
+      instance.options.pages.fetch.restore();
+    });
   });
 
   test.spec('modified (string path, array graph)', function () {
