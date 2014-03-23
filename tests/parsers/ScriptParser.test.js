c3dd973cd69de9f4e0e6ca12000bac55173f6064
adamrenklint <adam@renklint.com>
2014-03-23 19:22:10 +0100

    script change parser

--- tests/parsers/ScriptParser.test.js
+++ tests/parsers/ScriptParser.test.js
@@ -23,18 +23,27 @@ test([
 
   test.spec('parse (object model, string rendered, object dependencies)', function () {
 
-    test.it('should add model as a dependency of itself', function () {
+    test.it('should add model as a dependency of itself', function (done) {
 
       var model = new test.deps.Model({
-        'path': 'foo/bar.js'
+        'path': 'foo/bar.js',
+        'url': '/foo/bar.js'
       });
       var dependencies = new test.deps.Model();
 
       instance.parse(model, null, dependencies);
 
       expect(_.keys(dependencies.attributes).length).to.equal(1);
-      var first = dependencies.get('foo/bar.js')[0];
-      expect(first && first.attributes.path).to.equal('foo/bar.js');
+
+      var found = _.find(dependencies.attributes, function (graph, path) {
+        if (path.indexOf('foo/bar.js') >= 0) {
+          var inner = _.find(graph, function (node) {
+            if (node.attributes.path.indexOf('foo/bar.js') >= 0) {
+              done();
+            }
+          });
+        }
+      });
     });
   });
 });
