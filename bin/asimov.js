5200f2e2155baac584cc99c6f39851b2bccbab33
adamrenklint <adam@renklint.com>
2014-03-31 23:10:43 +0200

    added help command

--- bin/asimov.js
+++ bin/asimov.js
@@ -4,20 +4,46 @@ var Base = require('../lib/core/Base');
 var _super = Base.prototype;
 var pkg = require('../package.json');
 var _ = require('lodash');
-// var args = process.argv;
-// var pathIndex;
+var commandsPath = '../lib/cli/';
 
 var CLI = Base.extend({
 
-  'namespace': 'asimov',
+  'namespace': 'cli',
+
+  'paths': {
+
+    'create': commandsPath + 'Create',
+    'help': commandsPath + 'Help'
+  },
 
   'initialize': function () {
 
     var self = this;
     _super.initialize.apply(self, arguments);
 
-    self.logger.pending('main', 'Loading asimov.js @ ' + self.options.pkg.version);
-    self.logger.log('main', 'The time is ' + new Date());
+    self.logger.pending(self.namespace, 'Loading asimov.js @ ' + self.options.pkg.version);
+    self.logger.log(self.namespace, 'The time is ' + new Date());
+
+    var Command = self.getCommand(self.options.args);
+    self.assert('function', Command, 'Invalid command');
+    new Command(self.options);
+  },
+
+  'getCommand': function (args) {
+
+    var self = this;
+    var pathIndex;
+
+    _.find(args, function (arg, index) {
+      if (arg.indexOf('asimov.js/bin') > 0) {
+        pathIndex = index;
+      }
+    });
+
+    self.assert('number', pathIndex, 'Failed to start CLI, invalid load path');
+    var name = self.options.command = args[pathIndex + 1];
+    var loadPath = self.paths[name];
+    return require(loadPath);
   }
 });
 
@@ -25,70 +51,3 @@ module.exports = new CLI({
   'pkg': pkg,
   'args': process.argv
 });
-
-// console.log()
-
-// _.find(args, function (arg, index) {
-//   if (arg.indexOf('asimov.js/bin') > 0) {
-//     pathIndex = index;
-//   }
-// });
-
-// if (!pathIndex) {
-//   throw new Error('CLI started with invalid path');
-// }
-
-// var command = args[pathIndex + 1];
-
-// if (!command) {
-//   throw new Error('Must specify a command to use');
-// }
-
-// command = _.find(x, function (_command, name) {
-//   return name === command;
-// });
-
-// // var program = require('commander');
-// console.log(command);
-
-
-
-// // console.log('CLI no implemented yet, fork https://github.com/adamrenklint/asimovjs-template-project.git to get started');
-
-// // var requirejs = require('requirejs');
-// //
-// // var path = require('path');
-// // var baseDir = path.join(__dirname, '..');
-
-// // requirejs.config({
-// //   'paths': {
-// //     // 'asimov-framework': '../node_modules/asimov-framework',
-// //     // 'asimov-framework': '../../asimov-framework',
-// //     // 'asimov-core': '../node_modules/asimov-framework/node_modules/asimov-core/lib',
-// //     // 'vendor/wunderbits.core': '../node_modules/asimov-framework/node_modules/asimov-core/vendor/wunderbits.core'
-// //   }
-// // });
-
-// // function wrapAction (path) {
-// //   return function () {
-// //     var
-// //     // return requirejs(['../lib/actions/' + path], function (Action) {
-
-// //     //   return new Action({
-// //     //     'baseDir': baseDir,
-// //     //     'args': program.args[0]
-// //     //   });
-// //     // });
-// //   };
-// // }
-
-// // program
-// //   .version(meta.version);
-
-// // program
-// //   .command('create')
-// //   .option('--name [name]', 'Name of template')
-// //   .description('Preview template with [name]')
-// //   .action(wrapAction('Create'));
-
-// // program.parse(process.argv);
