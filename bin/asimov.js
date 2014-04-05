c71fe4038d6c61b112aba7d4ccaec43e0b990c36
adamrenklint <adam@renklint.com>
2014-04-05 13:20:32 +0200

    debugging travis

--- bin/asimov.js
+++ bin/asimov.js
@@ -15,6 +15,7 @@ var commandsPath = '../lib/commands/';
 var path = __dirname;
 var isModule = path.indexOf('node_modules/asimov.js/bin') >= 0;
 var frameworkDir = path.replace('/bin', '');
+console.log(frameworkDir)
 
 var CLI = Base.extend({
 
@@ -54,6 +55,8 @@ var CLI = Base.extend({
 
     var self = this;
     var pathIndex;
+
+    console.log(args)
 
     _.find(args, function (arg, index) {
       if (arg.indexOf('asimov.js/bin') > 0 || arg.indexOf('bin/asimov.js') > 0 || arg.indexOf('bin/ajs') > 0) {
