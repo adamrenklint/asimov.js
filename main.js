04f83b830842965385266fee691b1f9ef8b31c52
adamrenklint <adam@renklint.com>
2014-04-10 08:32:53 +0200

    fix env flag

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 
 // Override env flags
 // process.env.DEBUG = true;
-process.env.ENV = 'development';
+// process.env.ENV = 'development';
 
 var options = {};
 var asimov = require('./lib/asimov')(options);
