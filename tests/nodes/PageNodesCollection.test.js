8a884fba3c0d6a29acb928bd0ee494296f28a411
adamrenklint <adam@renklint.com>
2014-04-11 11:52:08 +0200

    all pagenodes collection tests passing, need to document

--- tests/nodes/PageNodesCollection.test.js
+++ tests/nodes/PageNodesCollection.test.js
@@ -25,18 +25,6 @@ Test.run('nodes/PageNodesCollection', function (test) {
     });
   });
 
-  // test.spec('fetch (array paths)', function () {
-
-  //   test.when('a path contains "/_"', function () {
-
-  //     test.it('should not add a pageNode and crawl', function () {
-
-  //       var children = instance.childrenOf('/hidden');
-  //       expect(children.models.length).to.equal(0);
-  //     });
-  //   });
-  // });
-
   test.spec('add (array models, object options)', function () {
 
     test.when('several unique models with similar ids are passed', function () {
@@ -191,11 +179,11 @@ Test.run('nodes/PageNodesCollection', function (test) {
           var offset = instance.filter(passIterator, {
             'sortBy': 'foo',
             'limit': 1,
-            'offset': 1
+            'offset': 4
           });
 
           expect(offset.length).to.equal(1);
-          expect(offset.models[0].attributes.url).to.equal('/zoo');
+          expect(offset.models[0].attributes.url).to.equal('/foo/bar');
         });
       });
     });
@@ -229,6 +217,7 @@ Test.run('nodes/PageNodesCollection', function (test) {
 
         test.it('should return each child in the PageNodesCollection', function () {
 
+          var now = new Date();
           var children = instance.childrenOf('/');
 
           expect(children.get('/foo').attributes.title).to.equal('foo');
