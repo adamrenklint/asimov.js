75d9997311036b2ba1d80a2a8fe49d8698506031
adamrenklint <adam@renklint.com>
2014-05-01 09:32:42 +0200

    add test class

---
+++ modules/asimov-test/lib/Test.js
@@ -0,0 +1,51 @@
+var Klass = require('../../asimov-core').Klass;
+var _super = Klass.prototype;
+
+var RunnerInterface = require('./RunnerInterface');
+var _ = require('lodash');
+var npath = require('path');
+var Mocha = require('mocha');
+var chai = require('chai');
+var colors = require('colors');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+var _super = Base.prototype;
+var mocha = new Mocha();
+
+var runner;
+
+var isSetup = false;
+function setup () {
+
+  if (isSetup) {
+    return;
+  }
+
+  isSetup = true;
+
+  runner = new RunnerInterface();
+
+  mocha.ui('bdd');
+
+  // Setup the reporter - some good ones: dot, spec, progress
+  mocha.reporter(process.env.REPORTER || 'spec');
+
+  chai.use(sinonChai);
+  global.should = chai.should();
+  global.expect = chai.expect;
+  global.sinon = sinon;
+}
+
+module.exports = Klass.extend({
+
+  'run': function (name, callback) {
+
+    var self = this;
+    var displayName = process.env.LEGACY_RENDER ? name.bold : name.black.inverse.bold;
+
+    setup();
+    describe(displayName, function () {
+      callback(runner);
+    });
+  }
+});
