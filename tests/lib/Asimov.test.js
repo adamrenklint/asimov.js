c2f0e887034528759258331241b493dba5d3ee1d
adamrenklint <adam@renklint.com>
2014-05-02 23:24:40 +0200

    starting to get the sequence runner going

--- tests/lib/Asimov.test.js
+++ tests/lib/Asimov.test.js
@@ -3,16 +3,16 @@ var Asimov = require('../../lib/Asimov');
 
 test('lib/Asimov', function (test) {
 
-  var instance;
+  var asimov;
 
   beforeEach(function () {
-    instance = new Asimov({
+    asimov = new Asimov({
       'muteLog': true
     });
   });
 
   afterEach(function () {
-    instance.destroy();
+    asimov.destroy();
   });
 
   test.spec('use (function plugin)', function () {
@@ -20,7 +20,7 @@ test('lib/Asimov', function (test) {
     test.when('plugin is not a function', function () {
 
       test.itShould.throwError(function () {
-        instance.use();
+        asimov.use();
       });
     });
 
@@ -29,15 +29,99 @@ test('lib/Asimov', function (test) {
       test.it('should execute the function', function () {
 
         var spy = sinon.spy();
-        instance.use(spy);
+        asimov.use(spy);
         expect(spy).to.have.been.calledOnce;
       });
 
       test.it('should pass itself to the function', function (done) {
 
-        instance.use(function (a) {
-          expect(a).to.equal(instance);
+        asimov.use(function (a) {
+          expect(a).to.equal(asimov);
           done();
+        });
+      });
+
+      test.it('should be chainable', function () {
+
+        var spy = sinon.spy();
+        expect(asimov.use(spy)).to.equal(asimov);
+      });
+    });
+  });
+
+  test.spec('init (function initializer)', function () {
+
+    test.when('initializer is not a function', function () {
+
+      test.itShould.throwError(function () {
+        asimov.init();
+      });
+    });
+
+    test.when('initializer is a function', function () {
+
+      test.it('should add initializer to the queue', function () {
+
+        var spy = sinon.spy();
+        asimov.init(spy);
+
+        expect(asimov.initializers[0]).to.equal(spy);
+      });
+
+      test.it('should only add the same initializer once', function () {
+
+        var spy = sinon.spy();
+        asimov.init(spy).init(spy);
+
+        expect(asimov.initializers.length).to.equal(1);
+      });
+
+      test.it('should be chainable', function () {
+
+        var spy = sinon.spy();
+        expect(asimov.init(spy)).to.equal(asimov);
+      });
+    });
+  });
+
+  test.spec('sequence (string namespace, function done)', function () {
+
+    test.when('namespace is not valid', function () {
+
+      test.itShould.throwError(function () {
+        asimov.sequence('beeswax');
+      });
+    });
+
+    test.when('namespace is valid', function () {
+
+      test.it('should execute the first job', function () {
+
+        var spy = sinon.spy();
+        asimov.init(spy);
+        asimov.sequence('initializer');
+
+        expect(spy).to.have.been.calledOnce;
+      });
+
+      test.it('should pass a "next" iterator function', function (done) {
+
+        asimov.init(function (next) {
+          expect(next).to.be.a('function');
+          done();
+        });
+        asimov.sequence('initializer');
+      });
+
+      test.when('the job executes the "next" iterator', function () {
+
+        test.it('should execute the next job', function () {
+
+        });
+
+        test.when('the last job calls the "next" iterator', function () {
+
+          test.it('should call the "done" callback');
         });
       });
     });
@@ -45,9 +129,16 @@ test('lib/Asimov', function (test) {
 
   // test.spec('start ()', function () {
   //
-  //   test.it('should execute all initializers', function () {
+  //   test.when('executing initializers', function () {
   //
+  //     test.it('should execute the first initializer', function)
   //
+  //     // test.it('should execute all initializers', function () {
+  //     //
+  //     //   var first = function (next) {
+  //     //
+  //     //   }
+  //     // });
   //   });
   // });
 });
