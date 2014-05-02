c9955814ce71427a40576b75997a3f831035101b
adamrenklint <adam@renklint.com>
2014-05-02 22:12:07 +0200

    implemeted .use, first passing tests

--- tests/lib/Asimov.test.js
+++ tests/lib/Asimov.test.js
@@ -1,11 +1,53 @@
 var test = require('asimov-test');
+var Asimov = require('../../lib/Asimov');
 
 test('lib/Asimov', function (test) {
 
-  test.spec(function () {
+  var instance;
 
-    test.it('should be true', function () {
-      expect(true).to.be.false;
+  beforeEach(function () {
+    instance = new Asimov({
+      'muteLog': true
     });
   });
+
+  afterEach(function () {
+    instance.destroy();
+  });
+
+  test.spec('use (function plugin)', function () {
+
+    test.when('plugin is not a function', function () {
+
+      test.itShould.throwError(function () {
+        instance.use();
+      });
+    });
+
+    test.when('plugin is a function', function () {
+
+      test.it('should execute the function', function () {
+
+        var spy = sinon.spy();
+        instance.use(spy);
+        expect(spy).to.have.been.calledOnce;
+      });
+
+      test.it('should pass itself to the function', function (done) {
+
+        instance.use(function (a) {
+          expect(a).to.equal(instance);
+          done();
+        });
+      });
+    });
+  });
+
+  // test.spec('start ()', function () {
+  //
+  //   test.it('should execute all initializers', function () {
+  //
+  //
+  //   });
+  // });
 });
